cd01fb1795d6abc29014162f6243cae0e779e8e9
AbdelrahmanAshraf318 <abdelrahmanashraf25102000@gmail.com>
2024-12-11 17:13:40 +0200

    Set all dependencies

---
+++ TO-DO-List/src/main/java/com/example/TO_DO/List/model/Todo.java
@@ -0,0 +1,17 @@
+package com.example.TO_DO.List.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+public class Todo
+{
+
+    private UUID id;
+    private String task; // this for the description of the task
+    private Boolean completed;
+    private LocalDateTime createdAt;
+
+
+
+}
