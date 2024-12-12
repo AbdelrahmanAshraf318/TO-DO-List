800d44c986cd6f76dafc392ae79540dda895a477
AbdelrahmanAshraf318 <abdelrahmanashraf25102000@gmail.com>
2024-12-12 11:53:02 +0200

    Add the Database Model (To-Do items)

--- TO-DO-List/src/main/java/com/example/TO_DO/List/model/Todo.java
+++ TO-DO-List/src/main/java/com/example/TO_DO/List/model/Todo.java
@@ -1,17 +1,63 @@
 package com.example.TO_DO.List.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-
+@Entity
 public class Todo
 {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
     private UUID id;
+    private String name;
     private String task; // this for the description of the task
     private Boolean completed;
+
+    @CreationTimestamp // Automatically sets the value to the current timesamp
+    @Column(updatable = false) //ENsures that the value is never updated
     private LocalDateTime createdAt;
 
+    public UUID getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getTask() {
+        return task;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
 }
